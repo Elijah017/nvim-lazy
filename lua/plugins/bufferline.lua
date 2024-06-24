e1ab1a1f66dea8de37d2da94692e392fad4de4da
elijah <elmmullens@gmail.com>
2024-06-24 13:14:09 +0800

    remove base to start again

--- lua/plugins/bufferline.lua
+++
@@ -1,9 +0,0 @@
-local bufferline = {
-  'akinsho/bufferline.nvim',
-  version = "*",
-  dependencies = 'nvim-tree/nvim-web-devicons',
-  lazy = true,
-  event = { 'BufNewFile', 'BufReadPre' },
-}
-
-return bufferline
